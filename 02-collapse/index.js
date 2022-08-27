8f7abbf38ff3c06e03893dde6f3c66d51ddf0271
Maksim <111878146+r-u-f-u-s@users.noreply.github.com>
2022-08-28 01:10:26 +0300

    update 2.1

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -4,6 +4,36 @@ let text = document.querySelector('.collapsible__content');
 let buttonNameVisible = document.querySelector('.collapsible__action--visible');
 let buttonNameHidden = document.querySelector('.collapsible__action--hidden');
 
+
+let shift = [
+    {transform: 'translate(-100%)'},
+    {transform: 'translate(0)'},
+]
+
+let timing = {
+    duration: 1500,
+    fill: "both"
+}
+text.style.cssText = 'visibility:hidden; width:530px'
+button.style.cssText = 'position:relative; width:155px; height:20px; display:flex; justify-content:center; align-items:center;'
+buttonNameVisible.style.cssText = 'position:absolute; width:140px; opacity:0'
+buttonNameHidden.style.cssText = 'position:absolute; width:140px;'
+
+button.addEventListener("click", function() {
+    if (buttonNameVisible.style.opacity == "0") {
+        buttonNameVisible.style.opacity = "1";
+        buttonNameHidden.style.opacity = "0";
+        text.style.visibility = "visible";
+        text.animate(shift, timing);
+    } else {
+        buttonNameVisible.style.opacity = "0";
+        buttonNameHidden.style.opacity = "1";
+        text.animate(shift, timing).reverse();
+    }
+});
+
+
+/*
 buttonNameVisible.style.display = "none";
 text.style.opacity = "0"
 
@@ -19,3 +49,4 @@ button.addEventListener("click", function() {
         text.style.opacity = "0";
     }
 });
+*/
